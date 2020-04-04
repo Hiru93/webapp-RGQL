a44b47fc4f6a3a38ae79dd88b7927aaf18cf1fd8
Hiru93 <mattia.gallinaro93@gmail.com>
2020-04-04 13:10:55 +0200

    .

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-import { auth } from './firebase/firevbase.utils';
+import { auth, createUserProfileDocument } from './firebase/firevbase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -25,10 +25,30 @@ class App extends React.Component {
 
   componentDidMount() {
     /** Create the subscription of a user */
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        /**
+         * From the firebase documentation
+         * 
+         * A DataSnapshot is an efficiently generated, immutable copy of the data at a Database location. 
+         * It cannot be modified and will never change (to modify data, you always call the set() method on a Reference directly)
+         */
+        userRef.onSnapshot(snapShot => {
+          /** Data as a snapshot from our firebase db */
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          },
+          console.log(this.state))
+        });
+      } else {
+        /** If the returned obj (our user) is null, we need to update our state according to that */
+        this.setState({ currentUser: userAuth })
+      }
     });
   }
 
--- src/firebase/firevbase.utils.js
+++ src/firebase/firevbase.utils.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { useRef } from 'react';
 
 const config = {
     apiKey: "AIzaSyAKo5b0C6nKY6tkkGSdkmZlWZazQkTE7Cw",
@@ -13,16 +14,55 @@ const config = {
     measurementId: "G-3953QFYTX1"
   };
 
-  firebase.initializeApp(config);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return;
+  /**
+   * From the firebase documentation
+   * 
+   * A Reference represents a specific location in your Database and can be used for reading or writing data to that Database location
+   */
+  const userRef = firestore.doc(`users/${ userAuth.uid }`);
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+  /**
+  * From the firebase documentation
+  * 
+  * A DataSnapshot is an efficiently generated, immutable copy of the data at a Database location. 
+  * It cannot be modified and will never change (to modify data, you always call the set() method on a Reference directly)
+  */
+  const snapShot = await userRef.get();
+  if(!snapShot.exsists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
 
-  /** Google auth setup using firebase */
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({ prompt: 'select_account' });
+    try {
+      /**
+       * We are using a reference to write information on our firebase db.
+       * Firebase references are the only object types that allow writing operations on databases
+       */
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user ', error.message);
+    }
+  }
 
-  /** Create the google popup when this provider is used */
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  return userRef;
+}
 
-  export default firebase;
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+/** Google auth setup using firebase */
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+/** Create the google popup when this provider is used */
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
