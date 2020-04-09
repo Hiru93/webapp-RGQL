9a1c60ce269628ffc84998ca5b516e634f268454
Mattia Gallinaro <mgallinaro@remedia.it>
2020-04-09 17:37:24 +0200

    .

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ collection }) => {
+    const { title, items } = collection;
+    return (
+        <div className="collection-page">
+            <h2 classNam="title">{ title }</h2>
+            <div className="items">
+                { items.map(item => (
+                    <CollectionItem key={ item.id } item={ item } />
+                )) }
+            </div>
+        </div>
+    );
+};
+
+/**
+ * ownProps indicates all the local props of the component
+ * so we can take the only props which exist inside the current component
+ */
+const mapStateToProps = (state, ownProps) => ({
+    /**
+     * We are using a selector which expect a parameter as input for the function
+     * the createSelector (written inside the selector file) is returning to us
+     * (the expected param is "collectionUrlParam" inside the selector definition file)
+     */
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -9,6 +9,8 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
 /**
  * key indicates the actual key inside the localstorage
@@ -23,7 +25,9 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
 });
 
 /**
